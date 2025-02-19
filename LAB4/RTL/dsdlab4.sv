d7c62265376569bd2a61346ec4cca3340db2ac25
Ammarahwakeel <ammarahwakeel9@gmail.com>
2025-02-19 02:18:32 -0800

    Add files via upload

---
+++ LAB4/RTL/dsdlab4.sv
@@ -0,0 +1,15 @@
+module dsdlab4(
+    output logic red,
+    output logic blue,
+    output logic green,// defining outputs
+    input logic a1,
+    input logic a0,
+    input logic b1,
+    input logic b0
+    
+    );
+    assign red= (~b1 & ~b0) | (a1&a0) | (~b1 & b0 & a0) | (~b1& b0 & a1) | (b1 & ~b0 & a1) ;
+    assign green= (~a1 & ~a0 ) | (~a1 & b0) | (~a1 & b1)| (b1 & b0) | (~a0 & b1);
+    assign blue= (a1&~b1) | (a0&~b0) | (~a0&b0) | (~a1&b1);
+    
+endmodule
